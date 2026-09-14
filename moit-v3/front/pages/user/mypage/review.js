dfe182a4e80fffa8cd596abef222386be5dcd411
mundaewon1 <less40@naver.com>
2026-09-14 10:59:20 +0900

    수정

--- moit-v3/front/pages/user/mypage/review.js
+++ moit-v3/front/pages/user/mypage/review.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
-import api from ".../api/axios";
+import api from '../../../api/axios';
 import {
   Button,
   Card,
